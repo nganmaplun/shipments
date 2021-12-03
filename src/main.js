92bcb826304d33a346e282383a5d082cc0560748
nganmaplun <nganmythanh1999@gmail.com>
2021-12-03 16:57:54 +0700

    Install boostrap, vue-router, axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import router from './router'
+createApp(App).use(router).mount('#app')
